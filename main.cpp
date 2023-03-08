b2e04705c5114fad521008505d8993dcb0717c04
Trev <trw60@georgetown.edu>
2023-03-08 13:15:50 -0500

    created mains .h and .cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "main.h"
+
+using namespace std;
+
+int main( int argc, const char* argv[] )
+{
+    cout << "TEST" << endl;
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,26 @@
+/*
+ *  main.h
+ *
+ *  COSC 160 Spring 2023
+ *  Project 2
+ *
+ *  Due on:    FEB 17, 2023
+ *
+ *  Author: Trevor Willard trw60
+ *
+ *
+ *  In accordance with the class policies and Georgetown's
+ *  Honor Code, I certify that, with the exception of the
+ *  class resources and those items noted below, I have neither
+ *  given nor received any assistance on this project.
+ *
+ *  References not otherwise commented within the program source code.
+ *  Note that you should not mention any help from the TAs, the professor,
+ *  or any code taken from the class textbooks.
+ *  
+ *  I affirm that the files submitted are the correct files that I want to be graded.
+ *  I have double-checked the submission and am certain that the zip file was
+ *  correctly created and contains the correct files for this assignment.
+ */
+
+#include <iostream>
