4bc4d2b31aeede9f33f4ee1e4604706db631efcd
Trev <trw60@georgetown.edu>
2023-03-12 18:45:25 -0400

    BST skeleton

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,2 +1,23 @@
 #include "BinarySearchTree.h"
 
+bstNode* BinarySearchTree::spotScout( bstNode *node, int data )
+{
+    // creates a node if BST is empty
+    if( node == NULL )
+    {
+        node = new bstNode;
+        node->data = data;
+        node->left = NULL;
+        node->right = NULL;
+    }
+    else if( data < node->data )
+    {
+        node->left = spotScout(node->left, data);
+    }
+    else if ( data > node->data ) 
+    {
+        node->right = spotScout(node->right, data);
+    }
+
+    return node;
+}
--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -6,12 +6,25 @@
 
 using namespace std;
 
+struct bstNode 
+{
+    int data;
+    bstNode* left;
+    bstNode* right;
+};
+
 class BinarySearchTree
 {
-
 private:
+    bstNode* root;
+
+    bstNode* spotScout( bstNode* node, int data );
+
 
 public:
-    
+    BinarySearchTree() { root = NULL; }
+
+    void insert( int data ) { root = spotScout( root, data ); }
+
     
 };
