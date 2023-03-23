8eaddc2e32647d6157dada199f4bc7826165d973
Trev <trw60@georgetown.edu>
2023-03-23 01:04:27 -0400

    initial tree out = 1 search of present = 1

--- main.cpp
+++ main.cpp
@@ -22,12 +22,7 @@ int main( int argc, const char* argv[] )
 
   while( portal1 >> slot)
   {
-    if( slot == '\n' )
-    {
-      portal1.ignore(1);
-      cout << "IGNORING YOU" << endl;
-    }
-    else if( slot == '$' )
+    if( slot == '$' )
     {
       break;
     }
@@ -45,45 +40,10 @@ int main( int argc, const char* argv[] )
       }
 
       value = stoi(one);
-      cout << "VALUE = " << value << endl;
       bst.insert(value);
     }
   }
 
-/*
-  bool notInt = true;
-
-  while( notInt )
-  {
-    if( portal1.peek() == '\n' || portal1.peek() == '$' )
-    {
-      portal1.ignore(1,'$');
-      cout << "IGNORING YOU" << endl;
-    }
-    else
-  }
-*/
-  while( portal1 >> slot )
-  {
-    if( slot == '\n' )
-    {
-      portal1.ignore(1);
-      cout << "IGNORING YOU" << endl;
-    }
-    else if( slot == '$' )
-    {
-      break;
-    }
-    else if( isdigit(slot) )
-    {
-      cout << "GOT A SEARCH, PAW" << endl;
-      string s(1, slot);
-      value = stoi(s);
-      cout << "VALUE = " << value << endl;
-      bst.search(value);
-    }
-  }
-  
   cout << "FIRST STATE - BINARY SEARCH TREE STRUCTURE OUTPUT: " << endl;
   cout << endl;
 
@@ -91,6 +51,38 @@ int main( int argc, const char* argv[] )
   bst.printPreorder(mainRoot);
   cout << endl;
   cout << endl;
+
+  while( portal1 >> slot )
+  {
+    if( slot == '$' )
+    {
+      break;
+    }
+    else if( isdigit(slot) )
+    {
+      one = slot;
+
+      fill = portal1.peek();
+
+      while( isdigit(fill) )
+      {
+        portal1 >> fill;
+        one = one + fill;
+        fill = portal1.peek();
+      }
+
+      value = stoi(one);
+      if( bst.search(value) )
+      {
+        cout << value << " LOCATED." << endl;
+      }
+      else
+      {
+        cout << value << " NOT FOUND." << endl;
+      }
+    }
+  }
+  
 
   portal1.close();
 
